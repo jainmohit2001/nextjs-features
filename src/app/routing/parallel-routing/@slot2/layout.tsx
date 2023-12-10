dfcbf701c16c1e473dc93cc8c04f798f6b41ef06
jainmohit2001 <jainmohit2001@gmail.com>
2023-12-10 23:15:32 +0530

    typo fix

--- src/app/routing/parallel-routing/@slot2/layout.tsx
+++ src/app/routing/parallel-routing/@slot2/layout.tsx
@@ -5,7 +5,7 @@ export default function Slot2Layout({
 }) {
   return (
     <div className="flex flex-1 flex-col gap-3 border-2 border-blue-500 p-3">
-      <p className="text-2xl">This is a layout for Slot1</p>
+      <p className="text-2xl">This is a layout for Slot2</p>
       {children}
     </div>
   )
